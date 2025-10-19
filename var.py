a3f9001ae1b35fa8d0c38e31b73bab6a5a051f0b
alexTrod <alexandra.fd1000@gmail.com>
2025-10-19 14:12:58 +0000

    get global vars in one place

---
+++ var.py
@@ -0,0 +1,11 @@
+# path
+BASE_URL = 'https://gamma-api.polymarket.com/'
+TAGS_ENDPOINT = 'tags'
+EVENTS_ENDPOINT = 'events'
+MARKETS_ENDPOINT = 'markets'
+
+# url fields
+limit_field = 100
+offset_field = 0
+order_field = 'createdAt'
+asc_field = True
